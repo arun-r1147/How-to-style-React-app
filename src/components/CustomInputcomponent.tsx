4729d7343510c01f368b227dc9f23e4bd5b8b2de
Arun <youngdeveloper37@gmail.com>
2024-05-21 20:20:24 +0530

    FIX :: Update custom input component with tailwind css

--- src/components/CustomInputcomponent.tsx
+++ src/components/CustomInputcomponent.tsx
@@ -1,12 +1,5 @@
-import { styled } from "styled-components";
 import { FC } from "react";
 
-interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  $invalid: boolean;
-}
-interface LabelProps {
-  $invalid: boolean;
-}
 interface CustomInputProps {
   label: string;
   invalid: boolean;
@@ -14,42 +7,33 @@ interface CustomInputProps {
   type: string;
   name: string;
   placeholder: string;
-  value:string,
+  value: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
-
-const Label = styled.label<LabelProps>`
-  display: block;
-  margin-bottom: 0.5rem;
-  font-size: 0.75rem;
-  font-weight: 700;
-  letter-spacing: 0.1em;
-  text-transform: uppercase;
-  color: ${({ $invalid }) => ($invalid ? "#f87171" : "#6b7280")};
-  text-align: left;
-`;
-
-const Input = styled.input<InputProps>`
-  width: 100%;
-  padding: 0.75rem 1rem;
-  line-height: 1.5;
-  background-color: ${({ $invalid }) => ($invalid ? "#fed2d2" : "#d1d5db")};
-  color: ${({ $invalid }) => ($invalid ? "#f87171" : "#374151")};
-  border-radius: 0.25rem;
-  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-  outline: none;
-  border: 1px solid ${({ $invalid }) => ($invalid ? "#f87171" : "transparent")};
-`;
 
 export const CustomInput: FC<CustomInputProps> = ({
   label,
   invalid,
   ...inputProps
 }) => {
+  let labelClass =
+    "block mb-2 text-xs font-bold tracking-wide uppercase outline-none";
+  if (invalid) {
+    labelClass += " text-red-600";
+  } else {
+    labelClass += " text-stone-300";
+  }
+  let inputClass =
+    "w-full px-3 py-2 leading-tight border rounded shadow bg-stone-200 outline-none text-gray-800";
+  if (invalid) {
+    inputClass += " bg-red-300 border-red-600 ";
+  } else {
+    inputClass += " bg-stone-300 border-gray-600";
+  }
   return (
     <>
-      <Label $invalid={invalid}>{label}</Label>
-      <Input $invalid={invalid} {...inputProps} />
+      <label className={labelClass}>{label}</label>
+      <input className={inputClass} {...inputProps} />
     </>
   );
 };
