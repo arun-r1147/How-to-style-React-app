385f0da9ccbcbae701a04ef7a0cf9e92970500fc
Arun <youngdeveloper37@gmail.com>
2024-05-21 20:20:45 +0530

    FIX :: Update Header component with tailwind css

--- src/components/Header.component.tsx
+++ src/components/Header.component.tsx
@@ -1,52 +1,17 @@
 import { FC } from "react";
-import logo from "../../public/logo.png";
-import { styled } from "styled-components";
-
-const Headers = styled.header`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  margin-top: 2rem;
-  margin-bottom: 2rem;
-  & img {
-    object-fit: contain;
-    margin-bottom: 2rem;
-    width: 11rem;
-    height: 11rem;
-  }
-
-  & h2 {
-    font-size: 1.5rem;
-    font-weight: 600;
-    letter-spacing: 0.4em;
-    text-align: center;
-    text-transform: uppercase;
-    color: #9a3412;
-    font-family: "Pacifico", cursive;
-    margin: 0;
-  }
-
-  & p {
-    text-align: center;
-    color: #a39191;
-    margin: 0;
-  }
-
-  @media (max-width: 768px) {
-    margin-bottom: 4rem;
-
-    & h2 {
-      font-size: 2.25rem;
-    }
-  }
-`;
+import logo from "/logo.png";
 
 export const Header: FC = () => {
   return (
-    <Headers>
-      <img src={logo} alt="canvas image" />
-      <h2>Let's learn React Styling</h2>
-    </Headers>
+    <header className="flex flex-col justify-center items-center mt-8 mb-8">
+      <img
+        className="object-contain mb-8 w-44 h-44 p-3"
+        src={logo}
+        alt="canvas image"
+      />
+      <h2 className="text-4xl font-semibold text-center m-0 uppercase text-amber-800 tracking-widest font-title">
+        Let's learn React Styling
+      </h2>
+    </header>
   );
 };
