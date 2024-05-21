95c443cd017e1a6d16403e99021a4a21cc33ce48
Arun <youngdeveloper37@gmail.com>
2024-05-21 20:45:13 +0530

    FIX :: Create Button Component

---
+++ src/components/Button.component.tsx
@@ -0,0 +1,27 @@
+import { FC, ReactNode } from "react";
+interface ButtonProps {
+  children: ReactNode;
+  onClickSubmission: () => void;
+  isDisabled: boolean;
+}
+
+export const Button: FC<ButtonProps> = ({
+  children,
+  onClickSubmission,
+  isDisabled,
+}) => {
+  let buttonClass =
+    "bg-yellow-600 cursor-pointer backdrop-blur-0 leading-tight px-8 py-4 font-semibold uppercase rounded text-stone-800 focus:outline-none hover:bg-yellow-500 ";
+  if (isDisabled) {
+    buttonClass += " cursor-not-allowed";
+  } else {
+    buttonClass += " cursor-pointer";
+  }
+  return (
+    <>
+      <button className={buttonClass} onClick={onClickSubmission}>
+        {children}
+      </button>
+    </>
+  );
+};
